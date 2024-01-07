f058a8cc054fac7cc750e91d9d460bf8de71710f
axelmark <axelmark@outlook.com>
2024-01-08 01:19:34 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,7 +5,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
@@ -20,21 +19,23 @@ public class HabrCareerParse {
     public static void main(String[] args) throws IOException {
         int pageNumber = 1;
         HabrCareerDateTimeParser timeParser = new HabrCareerDateTimeParser();
+        for (int i = pageNumber; i < 6; i++) {
+            System.out.println("страница " + i);
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            Connection connection = Jsoup.connect(fullLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                Element dateElement = row.select(".vacancy-card__date").first();
+                String dateChild = dateElement.child(0).attr("datetime");
 
-        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-        Connection connection = Jsoup.connect(fullLink);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element dateElement = row.select(".vacancy-card__date").first();
-            String dateChild = dateElement.child(0).attr("datetime");
-
-            LocalDateTime dateTime = timeParser.parse(dateChild);
-            String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), dateTime);
-            System.out.printf("%s %s%n", vacancyName, link);
-        });
+                LocalDateTime dateTime = timeParser.parse(dateChild);
+                String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), dateTime);
+                System.out.printf("%s %s%n", vacancyName, link);
+            });
+        }
     }
 }
