691f0c20155645fa852e9c00f4c68ad7db5243eb
axelmark <axelmark@outlook.com>
2024-01-08 23:03:36 +0300

    4. Схема таблицы Post [#1731]

--- src/main/properties/db/script.sql
+++ src/main/properties/db/script.sql
@@ -2,3 +2,11 @@ create table rabbit(
     id serial primary key,
     created_date timestamp
 );
+
+create table post(
+    id serial primary key,
+    title varchar(255),
+    link varchar(255) unique,
+    description text,
+    created timestamp
+);
